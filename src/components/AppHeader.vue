1ee4cf1b6616a7d199471aebdea6c9d81275b3f5
Jarkovato <anton.anatolitch@yandex.ru>
2022-09-04 19:57:50 +0300

    feat: added language selection

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-header elevated height-hint="150">
+    <q-toolbar>
+      <q-btn
+        flat
+        dense
+        round
+        icon="menu"
+        aria-label="Menu"
+        @click="this.$emit('toggleLeftDrawer')"
+      />
+      <q-toolbar-title> Pomodoro can </q-toolbar-title>
+      <q-select
+        v-model="lang"
+        :options="langOptions"
+        filled
+        borderless
+        emit-value
+        map-options
+        style="width: 140px"
+      />
+      <q-toggle
+        v-model="isDarkThemeEnabled"
+        color="white"
+        label="Dark theme"
+        @update:model-value="toggleDarkTheme"
+      />
+    </q-toolbar>
+  </q-header>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue';
+import { useQuasar } from 'quasar';
+
+import languages from 'quasar/lang/index.json';
+
+const appLanguages = languages.filter((lang) =>
+  ['en-US', 'ru'].includes(lang.isoName)
+);
+const langOptions = appLanguages.map((lang) => ({
+  label: lang.nativeName,
+  value: lang.isoName,
+}));
+
+export default defineComponent({
+  name: 'AppHeader',
+  setup() {
+    const $q = useQuasar();
+    const isDarkThemeEnabled = ref(false);
+    const lang = ref($q.lang.isoName);
+
+    watch(lang, (val) => {
+      import('quasar/lang/' + val).then((lang) => {
+        $q.lang.set(lang.default);
+      });
+    });
+
+    return {
+      isDarkThemeEnabled,
+      lang,
+      langOptions,
+      toggleDarkTheme(flag: boolean): void {
+        $q.dark.set(flag);
+      },
+    };
+  },
+});
+</script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,26 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title> Pomodoro can </q-toolbar-title>
-        <q-toggle
-          v-model="isDarkThemeEnabled"
-          color="white"
-          label="dark mode"
-          @update:model-value="toggleDarkTheme"
-        />
-      </q-toolbar>
-    </q-header>
-
+    <AppHeader @toggleLeftDrawer="toggleLeftDrawer"></AppHeader>
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
         <q-item-label header> Essential Links </q-item-label>
@@ -41,8 +21,8 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { useQuasar } from 'quasar';
 import EssentialLink from 'components/EssentialLink.vue';
+import AppHeader from 'components/AppHeader.vue';
 
 const linksList = [
   {
@@ -63,23 +43,18 @@ export default defineComponent({
   name: 'MainLayout',
 
   components: {
+    AppHeader,
     EssentialLink,
   },
 
   setup() {
-    const $q = useQuasar();
     const leftDrawerOpen = ref(false);
-    const isDarkThemeEnabled = ref(false);
 
     return {
       essentialLinks: linksList,
       leftDrawerOpen,
-      isDarkThemeEnabled,
       toggleLeftDrawer() {
         leftDrawerOpen.value = !leftDrawerOpen.value;
-      },
-      toggleDarkTheme(flag: boolean): void {
-        $q.dark.set(flag);
       },
     };
   },
